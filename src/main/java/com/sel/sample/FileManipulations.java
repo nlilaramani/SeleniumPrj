1bb4082daf5c0ec06699a98d578b096e1811e0bc
itexps <itexps@192.168.0.34>
2020-04-08 21:01:38 -0500

    Add Tests

---
+++ src/main/java/com/sel/sample/FileManipulations.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sel.sample;
+
+/**
+ *
+ * @author itexps
+ */
+public class FileManipulations {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //java.io.FileInputStream("xyz.csv");
+        
+    }
+    
+}
