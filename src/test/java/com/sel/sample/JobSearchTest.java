b497a140b6f8d841b258b6164fc7ebdc0637ff8f
itexps <itexps@192.168.0.34>
2021-09-14 15:10:12 -0500

    Initial Project structure

---
+++ src/test/java/com/sel/sample/JobSearchTest.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sel.sample;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class JobSearchTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    //driver = new FirefoxDriver();
+    driver=DriverUtils.getFirfoxDriver();
+    //driver=DriverUtils.getChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testJobSearch() throws Exception {
+    driver.get("https://www.google.com/");
+    if(isElementPresent(By.name("q"))){
+        driver.findElement(By.name("q")).clear();
+        driver.findElement(By.name("q")).sendKeys("Selenium Jobs");
+        driver.findElement(By.xpath("(//input[@name='btnK'])[2]")).click();
+    }else{
+        if(isAlertPresent()){
+            closeAlertAndGetItsText();
+        }
+    }
+    
+    //AssertTrue(isElementPresent(By.xpath("text()=results")))
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
